6d6be300667d44a81de954fadae373f34a400d00
vagrant <vagrant@homestead>
2020-07-11 17:46:10 +0000

    编辑个人资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Http\Requests\UserRequest;
 
 class UsersController extends Controller
 {
@@ -11,5 +12,17 @@ class UsersController extends Controller
     {
         $compact = compact('user');
         return view('users.show', $compact);
+    }
+
+    public function edit(User $user)
+    {
+        $compact = compact('user');
+        return view('users.edit', $compact);
+    }
+
+    public function update(UserRequest $request, User $user)
+    {
+        $user->update($request->all());
+        return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功！');
     }
 }
