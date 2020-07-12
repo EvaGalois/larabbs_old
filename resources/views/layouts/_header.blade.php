00f1e9bdba71ba352a83caf0a3b7101e350758e5
vagrant <vagrant@homestead>
2020-07-12 09:08:38 +0000

    用户可以创建话题

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -26,9 +26,14 @@
                 <li class="nav-item"><a href="{{ route('login') }}" class="nav-link">登录</a></li>
                 <li class="nav-item"><a href="{{ route('register') }}" class="nav-link">注册</a></li>
                 @else
+                <li class="nav-item">
+                    <a class="nav-link mt-1 mr-3 font-weight-bold" href="{{ route('topics.create') }}">
+                        <i class="fa fa-plus"></i>
+                    </a>
+                </li>
                 <li class="nav-item dropdown">
                     <a href="#" id="navbarDropdown" class="nav-link dropdown-toggle" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <img src="{{ Auth::user()->avatar }}" width="30px" height="30px" class="img-responsive img-circle">
+                        <img src="{{ Auth::user()->avatar }}" width="30px" height="30px" class="img-responsive img-circle" style="border-radius:50%;">
                         {{ Auth::user()->name }}
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
