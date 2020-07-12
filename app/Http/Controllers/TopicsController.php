af06261b15500ce9e053ca343f6249d1face4042
vagrant <vagrant@homestead>
2020-07-12 19:13:41 +0000

    活跃用户

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\TopicRequest;
 use App\Models\Category;
 use Auth;
+use App\Models\User;
 
 use App\Handlers\ImageUploadHandler;
 
@@ -18,10 +19,12 @@ class TopicsController extends Controller
         $this->middleware('auth', ['except' => ['index', 'show']]);
     }
 
-	public function index(Request $request, Topic $topic)
+	public function index(Request $request, Topic $topic, User $user)
 	{
 		$topics = $topic->withOrder($request->order)->with('user', 'category')->paginate(30);
-		return view('topics.index', compact('topics'));
+		$active_users = $user->getActiveUsers();
+        // dd($active_users);
+        return view('topics.index', compact('topics', 'active_users'));
 	}
 
     public function show(Request $request, Topic $topic)
